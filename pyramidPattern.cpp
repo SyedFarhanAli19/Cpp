286b629e1921112ff5482da95cd189d0299d372c
SyedFarhanAli19 <exoriousyt2250@gmail.com>
2024-10-10 15:22:04 +0530

    fixed the code of this file

--- pyramidPattern.cpp
+++ pyramidPattern.cpp
@@ -6,13 +6,13 @@ int main()
     int input = 0;
     cout << "Enter input" << endl;
     cin >> input;
-    for (int i = 0; i <= input; i++)
+    for (int i = 1; i <= input; i++) // this if for number of rows 
     {
-        for (int j = 0; j < (input - i); j++)
+        for (int j = 1; j <= (input - i); j++)// this is for printing space before star
         {
             cout << " ";
         }
-        for (int j = 0; j < (2 * i - 1); j++)
+        for (int j = 1; j <= (2 * i - 1); j++)// this is for printing star after space
         {
             cout << "*";
         }
